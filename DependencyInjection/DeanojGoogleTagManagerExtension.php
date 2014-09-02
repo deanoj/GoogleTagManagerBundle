46e525adb6a221aac8f4c4b43818827caf1c07d4
deanoj <deanoj@deanoj.co.uk>
2014-09-03 00:02:49 +0100

    configuration fixed

--- DependencyInjection/DeanojGoogleTagManagerExtension.php
+++ DependencyInjection/DeanojGoogleTagManagerExtension.php
@@ -14,7 +14,7 @@ use Symfony\Component\Yaml\Yaml;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class DeanojGoogleTagManagerExtension extends Extension implements PrependExtensionInterface
+class DeanojGoogleTagManagerExtension extends Extension
 {
     /**
      * {@inheritDoc}
@@ -24,17 +24,11 @@ class DeanojGoogleTagManagerExtension extends Extension implements PrependExtens
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $container->setParameter('deanoj_google_tag_manager.google_tag_manager_code', $config['google_tag_manager_code']);
+        //$container->setParameter('deanoj_google_tag_manager.google_tag_manager_code', $config['google_tag_manager_code']);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
     }
 
-    public function prepend(ContainerBuilder $container)
-    {
 
-        $config = Yaml::parse( __DIR__ . '/../Resources/config/config.yml');
-        $container->prependExtensionConfig('deanoj_google_tag_manager', $config);
-
-    }
 }
